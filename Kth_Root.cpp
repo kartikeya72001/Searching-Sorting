210147d5f094399754ff756ecdea9c6fd9d33ab9
Kartikeya Agarwal <kartikeya72001@gmail.com>
2021-03-01 17:11:28 +0530

    KthRoot

---
+++ Kth_Root.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[]) {
+    int t;
+    cin>>t;
+    while(t--){
+        int n, k;
+        cin>>n>>k;
+        
+    }
+    return 0;
+}
